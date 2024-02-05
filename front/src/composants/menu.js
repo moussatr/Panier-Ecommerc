62ab92fb85603f9c490c2e5d1b5d93b285895667
Zineddine-Harrar <Zineddine-Harrar@gmail.com>
2024-02-05 18:03:52 +0100

    deploy

--- front/src/composants/menu.js
+++ front/src/composants/menu.js
@@ -1,30 +1,84 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import menu from '../css/menu.css';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import menu from "../css/menu.css";
 
 function Menu({ isLoggedIn }) {
   return (
-    <div className='menu'>
+    <div className="menu">
       <ul>
-        <li><NavLink to="/home" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Accueil</NavLink></li>
-        <li><NavLink to="/panier" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Panier et Paiement</NavLink></li>
-        <li><NavLink to="/list-commande" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Détails commande</NavLink></li>
-        <li><NavLink to="/create-produit" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Créer un produit</NavLink></li>
+        <li>
+          <NavLink
+            to="/home"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            Accueil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/panier"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            Panier et Paiement
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/list-commande"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            Détails commande
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/create-produit"
+            className={({ isActive }) => (isActive ? "activeLink" : undefined)}
+          >
+            Créer un produit
+          </NavLink>
+        </li>
 
-        {isLoggedIn ?(
-              <>
-                 <li><NavLink to="/logout" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Se déconnecter</NavLink></li>
-              </>
-        ):(
-              <>
-                <li><NavLink to="/register" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>S'inscrire</NavLink></li>
-                <li><NavLink to="/" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Se connecter</NavLink></li>
-              </>
+        {isLoggedIn ? (
+          <>
+            <li>
+              <NavLink
+                to="/logout"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                Se déconnecter
+              </NavLink>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink
+                to="/register"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                S'inscrire
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "activeLink" : undefined
+                }
+              >
+                Se connecter
+              </NavLink>
+            </li>
+          </>
         )}
-        </ul>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;
